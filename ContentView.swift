c0d570425c614ac833954cb8774728b1689d5669
zenga123 <zenga85@naver.com>
2025-03-15 17:19:19 +0900

    초기 커밋

---
+++ ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  warikaClone
+//
+//  Created by musung on 2025/03/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ warikaCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  warikaCloneApp.swift
+//  warikaClone
+//
+//  Created by musung on 2025/03/15.
+//
+
+import SwiftUI
+
+@main
+struct warikaCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
